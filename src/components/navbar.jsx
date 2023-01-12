2eb327a067550ae4bfc5b2c6574aec6ae782388d
imkewlhuh <rafael.lopez0827@gmail.com>
2023-01-11 21:01:19 -0500

    updated links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,21 @@
 import { Grid, GridItem, Box } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
     return (
         <div className='navbar'>
             <Grid templateColumns='repeat(9, 1fr)'>
                 <GridItem colStart={5} colSpan={4} fontSize='x-large' >Welcome to Giffee!</GridItem>
-                <Link to={"trending"}>
-                    <GridItem>
-                        <Box as='button' borderRadius='md' bg='blue' color='white' px={5} h={9}>
-                            Trending
-                        </Box>
-                    </GridItem>
-                </Link>
+                <NavLink to={"trending"}>
+                    {({ isActive }) => (
+                        <GridItem>
+                            <Box as='button' borderRadius='md' bg={isActive ? "blue" : "white"} color={isActive ? "white" : "blue"} px={5} h={9}>
+                                Trending
+                            </Box>
+                        </GridItem>
+                    )
+                    }
+                </NavLink>
             </Grid>
         </div>
     )
